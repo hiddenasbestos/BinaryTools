154f1d069eb75f9ae72d907fddc2a5f4aa51715c
David Walters <contact@davidwaltersdevelopment.com>
2021-09-06 20:47:19 +0100

    tweak usage info for Data tool

--- Source/BinaryTools.cpp
+++ Source/BinaryTools.cpp
@@ -60,13 +60,13 @@ static Tool gTools[] =
 
 	{
 		"data", Data, "Convert a binary file into BASIC DATA statements.", "<file> <output> [-line start[,step]] [-cols width] [-compact]",
-		"  <file>     An input file to read.\n\n"
-		"  <output>   Text output for the DATA statements.\n\n"
-		"  -line      Specify the starting line number and step.\n"
-	    "             Default is line 1000 with step 10.\n\n"
-		"  -cols      Specify the maximum line length.\n"
-		"             Default is 40 columns, minimum is 20.\n\n"
-		"  -compact   Don't include a space after each comma.\n"
+		"  <file>      An input file to read.\n\n"
+		"  <output>    Text output for the DATA statements.\n\n"
+		"  -line L,S   Specify the starting line number and step.\n"
+	    "              Default is line 1000 with step 10.\n\n"
+		"  -cols W     Specify the maximum line length.\n"
+		"              Default is 40 columns, minimum is 20.\n\n"
+		"  -compact    Don't include a space after each comma.\n"
 	},
 
 	{
